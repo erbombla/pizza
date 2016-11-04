4a30ecf61ef38c57f15d89de5f7028b822994610
Eric Bomblatus <ebomblatus@gmail.com>
2016-11-04 10:32:06 -0700

    add input for quantity

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-function pizzaOrder(topping, size) {
-  this.topping = topping;
+function pizzaOrder(toppingAmount, size, quantity) {
+  this.toppingAmount = toppingAmount;
   this.size = size;
+  this.quantity = quantity;
 }
 
-pizzaOrder.prototype.cost = function(topping, size) {
+pizzaOrder.prototype.cost = function(toppingAmount, size, quantity) {
   var cost;
-  if ( size === 'med' && topping === 0) {
-    cost = 20;
-  } else if ( size === 'lg' && topping === 0) {
-    cost = 30;
-  } else if ( size === 'med' && topping > 0){
-    cost = (20 + topping);
+  if ( size === "med" && toppingAmount === 0) {
+    cost = 20 * quantity;
+  } else if ( size === "lg" && toppingAmount === 0) {
+    cost = 30 * quantity;
+  } else if ( size === "med" && toppingAmount > 0) {
+    cost = (20 + toppingAmount) * quantity;
   } else {
-    cost = (30 + topping);
+    cost = (30 + toppingAmount) * quantity;
   }
   return cost;
 }
@@ -20,18 +21,20 @@ pizzaOrder.prototype.cost = function(topping, size) {
 $(function() {
   $("form#orderForm").submit(function(event) {
     event.preventDefault();
-    var selectSize = $("input[name=sizeRadios]:checked").val();
+    var selectedSize = $("input[name=sizeRadios]:checked").val();
     var toppingChoice = [];
     $("input[name=toppingCheckbox]:checked").each( function() {
       toppingChoice.push($(this).val());
     });
-    var toppingAmmount = toppingChoice.length;
+    var toppingChoiceAmmount = toppingChoice.length;
+    var selectedQuantity = parseInt($('select[name=orderQuantity]').val());
     var newPizzaOrder;
-    newPizzaOrder = new pizzaOrder(selectSize, toppingAmmount);
+    newPizzaOrder = new pizzaOrder(selectedSize, toppingChoiceAmmount, selectedQuantity);
 
+    $("#orderQuantity").text(newPizzaOrder.quantity);
     $("#orderSize").text(newPizzaOrder.size);
     $("#pizzaTopping").text(toppingChoice);
-    $("#totalCost").text(newPizzaOrder.cost(selectSize, toppingChoice));
+    $("#totalCost").text(newPizzaOrder.cost(selectedSize, toppingChoiceAmmount, selectedQuantity));
     $("#pizzaOrder").show();
     return false;
   });
