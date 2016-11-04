3564ac26f0f0f805f2785d69f4f3672c2f784ee9
Eric Bomblatus <ebomblatus@gmail.com>
2016-11-04 15:24:59 -0700

    debug and fix constructor issue, but total calculation isn't functioning

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-function pizzaOrder(toppingAmount, size, quantity) {
-  this.toppingAmount = toppingAmount;
+function pizzaOrder(topping, size, quantity, cost) {
+  this.topping = topping;
   this.size = size;
   this.quantity = quantity;
 }
 
-pizzaOrder.prototype.cost = function(toppingAmount, size, quantity) {
-debugger; 
-  var cost;
-  if ( size === "medium" && toppingAmount === 0) {
+pizzaOrder.prototype.cost = function(topping, size, quantity) {
+  debugger;
+  var quantity = this.quantity;
+  var size = this.size; 
+  if ( size === "medium" && topping === 0) {
     cost = 20 * quantity;
-  } else if ( size === "large" && toppingAmount === 0) {
+  } else if ( size === "large" && topping === 0) {
     cost = 30 * quantity;
-  } else if ( size === "medium" && toppingAmount > 0) {
-    cost = (20 + toppingAmount) * quantity;
+  } else if ( size === "medium" && topping > 0) {
+    cost = (20 + topping) * quantity;
   } else {
-    cost = (30 + toppingAmount) * quantity;
+    cost = (30 + topping) * quantity;
   }
   return cost;
 }
 
 $(function() {
   $("form#orderForm").submit(function(event) {
-
     event.preventDefault();
     var selectedSize = $("input[name=sizeRadios]:checked").val();
-    var toppingChoice = [];
+    var selectedTopping = [];
     $("input[name=toppingCheckbox]:checked").each( function() {
-      toppingChoice.push($(this).val());
+      selectedTopping.push($(this).val());
     });
-    var toppingChoiceAmmount = toppingChoice.length;
-    var selectedQuantity = parseInt($('select[name=orderQuantity]').val());
-    var newPizzaOrder;
-    newPizzaOrder = new pizzaOrder(selectedSize, toppingChoiceAmmount, selectedQuantity);
+    // var newTopping = selectedTopping.length;
+    var newQuantity = parseInt($('select[name=orderQuantity]').val());
+    var newPizzaOrder = new pizzaOrder(selectedTopping, selectedSize, newQuantity);
 
     $("#orderQuantity").text(newPizzaOrder.quantity);
     $("#orderSize").text(newPizzaOrder.size);
-    // $("#pizzaTopping").text(toppingChoice);
-    $("#totalCost").text(newPizzaOrder.cost(selectedSize, selectedQuantity));
+    $("#pizzaTopping").text(selectedTopping);
+    $("#totalCost").text(newPizzaOrder.cost(newPizzaOrder));
     $("#pizzaOrder").show();
     return false;
   });
