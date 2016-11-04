51c3f3b2d34d4a839225e3d2acd227d7c6a77890
Eric Bomblatus <ebomblatus@gmail.com>
2016-11-04 09:33:29 -0700

    complete scripts but calculation doesn't work

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
-function Pizza(topping, size) {
+function pizzaOrder(topping, size) {
   this.topping = topping;
   this.size = size;
 }
 
-Pizza.prototype.cost = function(topping, size) {
+pizzaOrder.prototype.cost = function(topping, size) {
   var cost;
-  if
-   = this.topping + this.size;
-  return cost
-};
+  if ( size === 'med' && topping === 0) {
+    cost = 20;
+  } else if ( size === 'lg' && topping === 0) {
+    cost = 30;
+  } else if ( size === 'med' && topping > 0){
+    cost = (20 + topping);
+  } else {
+    cost = (30 + topping);
+  }
+  return cost;
+}
 
 $(function() {
-  $("form#order").submit(function(event)) {
+  $("form#orderForm").submit(function(event) {
     event.preventDefault();
-    $("#order").click(function(event)) {
-      var topping = parseInt($('topping').val());
-      var size = parseInt($('size').val());
-      var newPizza = new Pizza(topping, size);
-    }
-    $('#reciept').append('<p>Total: $' + newPizza.cost() + 'Thank you.</p>');
+    var selectSize = $("input[name=sizeRadios]:checked").val();
+    var toppingChoice = [];
+    $("input[name=toppingCheckbox]:checked").each( function() {
+      toppingChoice.push($(this).val());
+    });
+    var toppingAmmount = toppingChoice.length;
+    var newPizzaOrder;
+    newPizzaOrder = new pizzaOrder(selectSize, toppingAmmount);
+
+    $("#orderSize").text(newPizzaOrder.size);
+    $("#pizzaTopping").text(toppingChoice);
+    $("#totalCost").text(newPizzaOrder.cost(selectSize, toppingChoice));
+    $("#pizzaOrder").show();
+    return false;
   });
 });
